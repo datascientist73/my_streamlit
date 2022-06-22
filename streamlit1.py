ca49aebf797ab547fb08152f8fad114153983d7d
Михаил <106927907+puf2012@users.noreply.github.com>
2022-06-22 23:24:19 +0300

    Add files via upload

---
+++ streamlit1.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+
+
+st.write("""
+#  Простое приложение для цен на акции
+Показаны цена закрытия акций и объем продаж акцций Apple.
+""")
+
+#https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2022-6-20')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+#st. run /Users/mike/Desktop/main/my_streamlit/streamlit1.py
